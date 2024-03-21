b1c72f3c40e4f27056ca72f60a840e10c6fe2e06
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-03-21 03:07:39 -0700

    fixed firebase APIKEY from .env

--- client/src/utils/firebase.utils.js
+++ client/src/utils/firebase.utils.js
@@ -2,16 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // const config = require('./secrets.json');
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
-
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
-
+// 
 // const uri = process.env.MONGODB_URI;
 const uri = 'mongodb://127.0.0.1:27017'
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
