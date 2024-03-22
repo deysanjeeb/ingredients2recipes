31a2b09f02f576d5df43fc537cde618104025a6b
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-03-21 18:38:49 -0700

    changed server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = 8000;
 // const bodyParser = require('body-parser');
 app.use(cors());
 
